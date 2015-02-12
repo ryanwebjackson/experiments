784e8fbf9925b6f67513454964df6ce37a8913d8
Ryan Jackson <ryanjackson0y-@live.com>
2015-02-12 16:33:05 -0600

    Add - some Python examples for learning purposes

---
+++ proof_of_concept/python/range.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Ryan
+#
+# Created:     29/03/2012
+# Copyright:   (c) Ryan 2012
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    sum = 0
+    for i in range(10):
+        sum = sum +i
+        print sum
+    print sum
+
+
+if __name__ == '__main__':
+    main()
