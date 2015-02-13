d43a0c0b2f0a3ec476dc61ceae9a770d0e3c0bbc
Ryan Jackson <ryanjackson0y-@live.com>
2015-02-13 09:21:25 -0600

    Add - CPP example project - Math

---
+++ proof_of_concept/c-plusplus/Math/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: rjackson22
+ *
+ * Created on September 9, 2010, 2:22 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+// Declare variables
+double result;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    result = (2 + 3 * 4.0);
+    cout << result << endl;
+    cout << !5 << endl;
+    return 0;
+}
+
