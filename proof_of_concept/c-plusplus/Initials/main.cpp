4433892be878e2dba2ea8d45a53dabee1813769d
Ryan Jackson <ryanjackson0y-@live.com>
2015-02-13 09:21:08 -0600

    Add - CPP example project - Initials

---
+++ proof_of_concept/c-plusplus/Initials/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: rjackson22
+ * Description: Output my initials to the screen
+ * Created on September 2, 2010, 12:20 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Main entry point for the application
+ */
+int main() {
+    cout << "ASCII art rawx!" << endl << endl;
+    cout << "RRRRRR    W     W         J" << endl;
+    cout << "R    R    W  W  W         J" << endl;
+    cout << "RRRRR     W  W  W         J" << endl;
+    cout << "R   R     W W W W    J    J" << endl;
+    cout << "R    R    W     W     JJJJ " << endl;
+    cout << endl;
+    return 0;
+}
+
